e7a0002cf47ccd84bff0587f027c1ec97c4ae2d2
Kevin Jacobs <jacobs@bioinformed.com>
2015-06-23 07:41:03 -0600

    Preliminary support for Python 3

--- vgraph/bed.py
+++ vgraph/bed.py
@@ -1,4 +1,24 @@
-from itertools import izip
+# -*- coding: utf-8 -*-
+
+## Copyright 2015 Kevin B Jacobs
+##
+## Licensed under the Apache License, Version 2.0 (the "License"); you may
+## not use this file except in compliance with the License.  You may obtain
+## a copy of the License at
+##
+##        http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+## WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+## License for the specific language governing permissions and limitations
+## under the License.
+
+
+try:
+    from itertools import izip as zip
+except ImportError:
+    pass
 
 from vgraph.smartfile import smartfile
 
@@ -49,7 +69,7 @@ class BedRecord(object):
         return line.rstrip()
 
     def __repr__(self):
-        fields = ('%s=%r' % (k, v) for k, v in izip(self.field_names, self.to_tuple()) if v not in (None, ''))
+        fields = ('%s=%r' % (k, v) for k, v in zip(self.field_names, self.to_tuple()) if v not in (None, ''))
         return 'BedRecord(%s)' % ', '.join(fields)
 
 
